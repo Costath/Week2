c7423a0491a5addcf7c55dc628e3000b623afc6f
Costath <thalesdaniel@gmail.com>
2019-05-08 16:21:14 -0400

    Week 2 class

---
+++ DateDemo/Date.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateDemo
+{
+    class Date
+    {
+        int year;
+        int month;
+        int day;
+
+        public Date(int year, int month, int day)
+        {
+            this.year = year;
+            this.month = month;
+            this.day = day;
+        }
+        public override string ToString()
+        {
+            return $"{year,4}/{month,2}/{day,2}";
+        }
+        public void Add(int days)
+        {
+            day += days;
+
+            Normalize();
+        }
+        public void Add(int month, int days)
+        {
+            this.month += month;
+            day += days; //or this.day += days;
+
+            Normalize();
+        }
+        public void Add(Date other)
+        {
+            day += other.day;
+            month += other.month;
+            year += other.year;
+
+            Normalize();
+        }
+        private void Normalize()
+        {
+            while(this.day > 30)
+            {
+                this.month++;
+                this.day -= 30;
+            }
+            while (this.month > 12)
+            {
+                this.year++;
+                this.month -= 12;
+            }
+        }
+    }
+}
---
+++ DateDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Date D = new Date(1992, 07, 23);
+            Console.WriteLine(D);
+
+            D.Add(10);
+            Console.WriteLine(D);
+
+            D.Add(5, 5);
+            Console.WriteLine(D);
+
+            Date D2 = new Date(1, 25, 25);
+            D.Add(D2);
+            Console.WriteLine(D2);
+            Console.WriteLine(D);
+
+            Date DDD = new Date(2019, 5, 8);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(DDD);
+            DDD.Add(365);
+            Console.WriteLine(DDD);
+
+        }
+    }
+}
